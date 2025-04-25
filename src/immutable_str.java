4e7fe2b82cb0885b2804dba0bfc0a5773a33c54b
kamlesh <sahukamleshkumar404@gmail.com>
2025-04-25 10:30:46 +0530

    Initial commit

---
+++ src/immutable_str.java
@@ -0,0 +1,10 @@
+public class immutable_str {
+    public static void main(String[] args) {
+
+        StringBuilder str=new StringBuilder();
+        str.append ("hello hiii byeee");
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.capacity());
+    }
+}
