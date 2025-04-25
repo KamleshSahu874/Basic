e9226900eed45b4e6192fec98a415ce44a61e1a7
kamlesh <sahukamleshkumar404@gmail.com>
2025-04-25 11:10:56 +0530

    Initial commit

---
+++ src/Return_type.java
@@ -0,0 +1,11 @@
+public class Return_type {
+    static int sum(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(sum(4));
+        int output=sum(4,7);
+        System.out.println(output);
+    }
+}
